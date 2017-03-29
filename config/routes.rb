d41d4e5f90db4700823d845b3aa7b935e6b58fc2
Lucas Timotio <lucastimotio@gmail.com>
2017-03-29 19:31:58 -0300

    #03

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :series do
+    resources :comments
+  end
+
+  resources :comments
   resources :series
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327231801) do
+ActiveRecord::Schema.define(version: 20170329040759) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "serie_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["serie_id"], name: "index_comments_on_serie_id"
+  end
 
   create_table "series", force: :cascade do |t|
     t.string   "name"
